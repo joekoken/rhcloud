99cbbb7d6173df5218b508d40c754a777b4b6b9f
joekoken <joekoken@hotmail.com>
2017-09-06 17:08:54 +0800

    test

---
+++ test.php
@@ -0,0 +1,233 @@
+<div class="scroll-menu teacher-user-guide">
+	<h4 class="panel-group-heading">Blackboard Mobile Student</h4>
+	<div aria-multiselectable="true" class="panel-group" id="accordion" role="tablist">
+		<div class="panel panel-default">
+			<div class="panel-heading" id="heading-Blackboard_Mobile_Student1" role="tab">
+				<h4 class="panel-title"><a aria-controls="Blackboard_Mobile_Student1" aria-expanded="false" class="collapsed" data-toggle="collapse" href="#Blackboard_Mobile_Student1" role="button">Introduction</a></h4>
+			</div>
+			<div aria-expanded="false" aria-labelledby="heading-Blackboard_Mobile_Student1" class="panel-collapse collapse" id="Blackboard_Mobile_Student1" role="tabpanel">
+				<div class="panel-body"><strong>Blackboard Mobile Student</strong> <strong>Teaching and learning, ready when you are</strong>
+					<p>Whether you’re submitting or grading assignments on the go, contributing to the class <span><span>blogs</span></span>, checking or posting grades – everything you need is at your fingertips. <em>Connect wherever and whenever you choose.</em></p>
+					<div> </div>
+					<p class="jbox"><img alt="Blackboard Mobile" src="images/blackboard_Guide/bbmob1(1).png" /></p>
+					 
+					<p><strong>Blackboard Student</strong></p>
+					<p>Bb Student is Blackboard's first mobile app centered around one type of user: students. Get ready for a completely new experience and look for your mobile learning! The app provides an intuitive way for you to interact with courses, content, instructors, and other students. Use it throughout your academic journey to stay on top of coursework and plan ahead.</p>
+					<p>An app designed just for you...students only!</p>
+					<p>With Bb Student, you can do the following:</p>
+					<ul style="margin-left: 40px;">
+						<li>Enjoy a modern, easy-to-use design.</li>
+						<li>Receive updates about your courses, content, and work due in the activity stream.</li>
+						<li>Take assignments, tests, and quizzes right from the app and review your submissions.</li>
+						<li>View your course grades as well as individual test and assignment grades.</li>
+						<li>Stay on task with a list of all due dates.</li>
+						<li>Collaborate with your class through video conferencing on iOS and Android</li>
+						<li>Attach assignment files from Google Drive, Dropbox, and OneDrive</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading" id="heading-Blackboard_Mobile_Student2" role="tab">
+				<h4 class="panel-title"><a aria-controls="Blackboard_Mobile_Student2" aria-expanded="false" class="collapsed" data-toggle="collapse" href="#Blackboard_Mobile_Student2" role="button">Install App and Log In</a></h4>
+			</div>
+			<div aria-expanded="false" aria-labelledby="heading-Blackboard_Mobile_Student2" class="panel-collapse collapse" id="Blackboard_Mobile_Student2" role="tabpanel">
+				<div class="panel-body">
+					<p><strong><img alt="B B student mobile app" src="images/bbmobile/bbapp.png" style="float: right;" /></strong> <strong>Use these steps to download and install the Blackboard app.</strong></p>
+					<ol>
+						<li>From your device, access the appropriate online store.</li>
+					</ol>
+					<ul style="margin-left: 80px;">
+						<li><a class="link-https 0" href="https://itunes.apple.com/app/blackboard-app/id950424861?mt=8" target="_blank" rel="nofollow">Download on your iPhone® or iPod touch®<span class="0"><span class="element-invisible"> </span></span></a><br /><img src="images/bbmobile/apple.png" /></li>
+						<li><a class="link-https 0" href="https://play.google.com/store/apps/details?id=com.blackboard.android.bbstudent&amp;hl=en" target="_blank" rel="nofollow">Download on your Android™ device<span class="0"><span class="element-invisible"> </span></span></a><br /><img src="images/bbmobile/google.png" /></li>
+						<li><a class="external 0" href="https://www.microsoft.com/store/p/blackboard/9nblggh08j8w?rtc=1" target="_blank" rel="nofollow">Download on your Windows® device<span class="0"><span class="element-invisible"> </span></span></a><br /><img src="images/bbmobile/microsoft.jpg" /></li>
+					</ul>
+					<ol start="2">
+						<li>If necessary, search for Blackboard.<br />If you search the App Store from an iPad, be sure to change the iPad Only filter to iPhone Only to find the app. <img src="images/bbmobile/bbsearch.jpg" /></li>
+						<li>Install the Blackboard app on your mobile device.</li>
+						<li>Open the Blackboard app and search for "The Hong Kong Polytechnic University".</li>
+						<li>Log in with your Blackboard Learn username and password. These are typically the same credentials you use to access your Blackboard courses on a computer.</li>
+					</ol>
+					<p><strong>Introducing the Blackboard App</strong> </p>
+					<p><iframe width="560" height="315" allowfullscreen="allowfullscreen" frameborder="0" src="https://www.youtube.com/embed/VT7MRvcZvsw"></iframe></p>
+					 <br />  
+					<p><strong>The Blackboard App Tour</strong></p>
+					<p><iframe width="560" height="315" allowfullscreen="allowfullscreen" frameborder="0" src="https://www.youtube.com/embed/iYO9y4j3njQ"></iframe></p>
+				</div>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading" id="heading-Blackboard_Mobile_Student3" role="tab">
+				<h4 class="panel-title"><a aria-controls="Blackboard_Mobile_Student3" aria-expanded="true" class="collapsed" data-toggle="collapse" href="#Blackboard_Mobile_Student3" role="button">Feature Guide</a></h4>
+			</div>
+			<div aria-expanded="true" aria-labelledby="heading-Blackboard_Mobile_Student3" class="panel-collapse collapse" id="Blackboard_Mobile_Student3" role="tabpanel">
+				<div class="panel-body"><strong>Navigation and Notifications</strong>
+					<div class="nbox">
+						<div class="row kbox" style="color: white; background-color: #186db7; text-align: center;">
+							<div class="col-xs-3"><strong>Feature</strong></div>
+							<div class="col-xs-9"><strong>Description</strong></div>
+						</div>
+						<div class="row kbox">
+							<div class="col-xs-3"><strong>Navigation</strong></div>
+							<div class="col-xs-9">We put your most-used features all in one place. The base navigation gives you easy access to course updates, view course content, and check your grades.<br /> <br /> <strong>Swipe down</strong> to go to a previous screen. When you expand the navigation, <strong>breadcrumbs</strong> appear at the bottom of the screen.</div>
+						</div>
+						<div class="row kbox">
+							<div class="col-xs-3"><strong>Activity Stream</strong></div>
+							<div class="col-xs-9">See an up-to-the-minute list of important course content and actions you need to take. The activity stream automatically prioritizes this content to help you focus on the tasks that you want to take care of right away.<br /> <br /> Tap an item in the stream to open it and begin working. Click the linked course title within the item if you want to go to the course outline instead.</div>
+						</div>
+						<div class="row kbox">
+							<div class="col-xs-3"><strong>Due Dates</strong></div>
+							<div class="col-xs-9">See upcoming assignments and tests across all of your courses. Work is color coded by course and grouped by day and week. You can quickly see what's due and prioritize your work.<br /> <br /> Note that tests and assignments show in this list only if your instructor specified due dates for them.</div>
+						</div>
+					</div>
+					<br /> <br /> <strong>Courses and People</strong>
+					<div class="nbox">
+						<div class="row kbox" style="color: white; background-color: #186db7; text-align: center;">
+							<div class="col-xs-3"><strong>Feature</strong></div>
+							<div class="col-xs-9"><strong>Description</strong></div>
+						</div>
+						<div class="row kbox">
+							<div class="col-xs-3"><strong>Profiles and Tiles</strong></div>
+							<div class="col-xs-9">
+								<div>Your Bb Student profile is populated with your Blackboard Learn cloud profile information, including your name and avatar. You can easily change your avatar in the app and replace all instances of your avatar in Bb Student and Learn. To learn more about profiles, see<a href="https://en-us.help.blackboard.com/Bb_Student/060_FAQs_-_Features" title="FAQs - Features">FAQs - Features</a>.</div>
+							Your profile also includes tiles that show which school or schools you attend. In the future, you will be able to add more tiles yourself.</div>
+						</div>
+						<div class="row kbox">
+							<div class="col-xs-3"><strong>Courses</strong></div>
+							<div class="col-xs-9">View all of your courses—past, present, and future. Tap a course to view its content and assignments. To learn more, see <a href="https://en-us.help.blackboard.com/Bb_Student/Course_List" title="Course List">Course List</a>.</div>
+						</div>
+						<div class="row kbox">
+							<div class="col-xs-3"><strong>Course Content</strong></div>
+							<div class="col-xs-9">
+								<div>Easily scan course materials and access what you need. Tap a piece of content to view its full details.</div>
+							You can view Microsoft<sup>®</sup> Word, Excel<sup>®</sup>, PowerPoint<sup>®</sup>, and PDF content that your instructors include in courses.</div>
+						</div>
+						<div class="row kbox">
+							<div class="col-xs-3"><strong>Collaborate</strong></div>
+							<div class="col-xs-9">Video conferencing is a fundamental part of Bb Student. In iOS and Android, you can participate in virtual sessions by using Collaborate with the Ultra Experience. To learn more, see <a href="https://en-us.help.blackboard.com/Bb_Student/Collaborate_in_Bb_Student" title="Collaborate in Bb Student">Collaborate in Bb Student</a>.</div>
+						</div>
+						<div class="row kbox">
+							<div class="col-xs-3"><strong>Cloud Content</strong></div>
+							<div class="col-xs-9">In assignments, you can attach files from your cloud file storage. You can connect Bb Student to Google Drive, OneDrive, and Dropbox for easy access to your files within the app. To learn more see <a href="https://en-us.help.blackboard.com/Bb_Student/Link_to_Cloud_Storage" title="Link to Cloud Storage">Link to Cloud Storage</a>.</div>
+						</div>
+					</div>
+					<div>*Organizations are not supported at this time.</div>
+					<div> </div>
+					<div><br /> <strong>Assignments, Tests, and Grades</strong></div>
+					<div class="nbox">
+						<div class="row kbox" style="color: white; background-color: #186db7; text-align: center;">
+							<div class="col-xs-3"><strong>Feature</strong></div>
+							<div class="col-xs-9"><strong>Description</strong></div>
+						</div>
+						<div class="row kbox">
+							<div class="col-xs-3"><strong>Assignments</strong></div>
+							<div class="col-xs-9">View, complete, and submit assignments right from the app.</div>
+						</div>
+						<div class="row kbox">
+							<div class="col-xs-3"><strong>Tests</strong></div>
+							<div class="col-xs-9">Tests with the following question types are taken within Bb Student: Multiple Choice, True / False, Either / Or, Short Answer, and Essay. If tests contain other question types, Bb Student directs you to a web view of Learn to complete the test.</div>
+						</div>
+						<div class="row kbox">
+							<div class="col-xs-3"><strong>Grades</strong></div>
+							<div class="col-xs-9">You get real-time access to grades and feedback within an individual course or from the Grades in the base navigation.</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="scroll-menu teacher-user-guide">
+	<h4 class="panel-group-heading">Blackboard Mobile Teacher</h4>
+	<div aria-multiselectable="true" class="panel-group" id="accordion" role="tablist">
+		<div class="panel panel-default">
+			<div class="panel-heading" id="heading-Blackboard_Mobile_Teacher1" role="tab">
+				<h4 class="panel-title"><a aria-controls="Blackboard_Mobile_Teacher1" aria-expanded="true" class="collapsed" data-toggle="collapse" href="#Blackboard_Mobile_Teacher1" role="button">Introduction</a></h4>
+			</div>
+			<div aria-labelledby="heading-Blackboard_Mobile_Teacher1" class="panel-collapse collapse" id="Blackboard_Mobile_Teacher1" role="tabpanel">
+				<div class="panel-body"><strong>Teaching and learning, ready when you are</strong>
+					<p>Whether you’re submitting or grading assignments on the go, contributing to the class <span><span>blogs</span></span>, checking or posting grades – everything you need is at your fingertips. <em>Connect wherever and whenever you choose.</em></p>
+					 
+					<p class="jbox"><img alt="Blackboard Mobile" src="images/blackboard_Guide/bbmob1(1).png" /></p>
+					 
+					<p><span style="color: #008000;"><b>Useful Documents</b></span></p>
+					<p>Select the links below to find further information on how to effectively use Blackboard Mobile Learn:</p>
+					<p><a alt="Introducing Mobile Learn" href="https://learn.polyu.edu.hk/bbcswebdav/pid-1556117-dt-content-rid-7612535_1/xid-7612535_1" target="_blank" title="Introducing Mobile Learn">Introducing Mobile Learn</a><a href="https://polyuit-my.sharepoint.com/personal/hanchan_polyu_edu_hk/_layouts/15/guestaccess.aspx?guestaccesstoken=hMYlzFwXsUv7zeJK1JQaLAB2K05O0Buj6iK0xRw1W8U%3d&amp;docid=0e5fcce49122e44dca2788886333edebe"> </a></p>
+					<p><a alt="Mobile Learn Feature Guide" href="https://polyuit-my.sharepoint.com/personal/hanchan_polyu_edu_hk/_layouts/15/guestaccess.aspx?guestaccesstoken=Od5pqxQ0fdAg35cnw0reXAZAx70MNN4vA7DtzCDE2Vk%3d&amp;docid=0fa471706a55c4809831cbe2c3fa3d666" target="_blank" title="Mobile Learn Feature Guide">Mobile Learn Feature Guide</a> </p>
+					<p><a alt="Guide to Mobile Tests" href="https://polyuit-my.sharepoint.com/personal/hanchan_polyu_edu_hk/_layouts/15/guestaccess.aspx?guestaccesstoken=Tj4i11%2bxpvM0lkjayzn4jD1caw4%2fTzN6QdO9hKKjaM4%3d&amp;docid=08942bf7d8620413cb743cebf1f25bec9" target="_blank" title="Guide to Mobile Tests">Guide to Mobile Tests</a> </p>
+				</div>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading" id="heading-Blackboard_Mobile_Teacher2" role="tab">
+				<h4 class="panel-title"><a aria-controls="Blackboard_Mobile_Teacher2" aria-expanded="false" class="collapsed" data-toggle="collapse" href="#Blackboard_Mobile_Teacher2" role="button">Demonstration</a></h4>
+			</div>
+			<div aria-labelledby="heading-Blackboard_Mobile_Teacher2" class="panel-collapse collapse" id="Blackboard_Mobile_Teacher2" role="tabpanel">
+				<div class="panel-body">
+					<p><strong>Blackboard Mobile Learn 4.0 iPhone/iPod Touch Demo Video</strong></p>
+					 
+					<p><iframe width="560" height="315" frameborder="0" src="https://www.youtube.com/embed/sWOjmu0QWvw"></iframe></p>
+				</div>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading" id="heading-Blackboard_Mobile_Teacher3" role="tab">
+				<h4 class="panel-title"><a aria-controls="Blackboard_Mobile_Teacher3" aria-expanded="false" class="collapsed" data-toggle="collapse" href="#Blackboard_Mobile_Teacher3" role="button">How to Download &amp; Use Blackboard Mobile Learn</a></h4>
+			</div>
+			<div aria-labelledby="heading-Blackboard_Mobile_Teacher3" class="panel-collapse collapse" id="Blackboard_Mobile_Teacher3" role="tabpanel">
+				<div class="panel-body">Find out how to download and use Blackboard Mobile Learn. See what tools are available via Mobile Learn and how you can use these in your teaching and learning.</div>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading" id="heading-Blackboard_Mobile_Teacher4" role="tab">
+				<h4 class="panel-title"><a aria-controls="Blackboard_Mobile_Teacher4" aria-expanded="false" class="collapsed" data-toggle="collapse" href="#Blackboard_Mobile_Teacher4" role="button">How to Find the App</a></h4>
+			</div>
+			<div aria-labelledby="heading-Blackboard_Mobile_Teacher4" class="panel-collapse collapse" id="Blackboard_Mobile_Teacher4" role="tabpanel">
+				<div class="panel-body">
+					<div style="border: thin solid #fffff; border-top-left-radius: 10px; border-top-right-radius: 10px; border-bottom-right-radius: 10px; border-bottom-left-radius: 10px; width: 95%; float: left; box-shadow: #fffff 3px 3px 3px; padding: 15px; margin: 10px; overflow: hidden;">
+						<b>"Blackboard Mobile Learn"</b> will be removed this app in store on <b>1 Aug</b> and will not support at the end of Aug.
+						<b>"Blackboard Instructor"</b> will be replaced it for teacher.<br><br>
+						
+						 
+						
+						<img src="images/bbmobile/blackboard_instructor.jpg"><br><br>
+						
+						To know more information about Blackboard Instructor: <a href="https://help.blackboard.com/Blackboard_App/Instructor" target="_blank">https://help.blackboard.com/Blackboard_App/Instructor</a>
+						<br><br>
+						Download link for Blackboard Instructor:<br>
+						
+						<div class="col-xs-5">
+							<img src="images/bbmobile/apple.png"><br>
+							
+							<a href="https://itunes.apple.com/app/blackboard-instructor/id1088457824?ls=1&mt=8" target="_blank" >Download on your Apple® device</a>
+						</div>
+						<div class="col-xs-5">
+							<img src="images/bbmobile/google.png"><br>
+							
+							<a href="https://play.google.com/store/apps/details?id=com.blackboard.android.bbinstructor" target="_blank" >Download on your Android™ device</a>
+						</div>
+						
+						
+						<div> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading" id="heading-Blackboard_Mobile_Teacher5" role="tab">
+				<h4 class="panel-title"><a aria-controls="Blackboard_Mobile_Teacher5" aria-expanded="false" class="collapsed" data-toggle="collapse" href="#Blackboard_Mobile_Teacher5" role="button">Blackboard Grader</a></h4>
+			</div>
+			<div aria-labelledby="heading-Blackboard_Mobile_Teacher5" class="panel-collapse collapse" id="Blackboard_Mobile_Teacher5" role="tabpanel">
+				<div class="panel-body">
+					<p><strong>Blackboard Grader</strong></p>
+					<p><br /> <em><span>Grade that pile of virtual papers with the <span>Blackboard</span> Grader <span><span><span>app</span></span></span></span></em></p>
+					<p><span>Educator's don’t have time to waste – we know you do many jobs simultaneously, and are specialists in multitasking. Now, grading student assignment submissions doesn't have to wait: Blackboard Grader <span><span><span>app</span></span></span> lets you do two things at once, anywhere you find yourself.</span></p>
+					<p><iframe width="560" height="315" frameborder="0" src="https://www.youtube.com/embed/6Me8Z0YSkn0"></iframe></p>
+					<p><strong>Features</strong></p>
+					<p><img alt="" src="images/blackboard_Guide/bbgrader.png" /></p>
+					<p><strong>Download Blackboard Grader to your mobile device via the link below or through your providers 'App' Store.</strong> <br /> <a href="https://itunes.apple.com/gb/app/bb-grader/id897611355?mt=8" target="_blank">itunes.apple.com gb app bb-grader/id897611355?mt=8</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
